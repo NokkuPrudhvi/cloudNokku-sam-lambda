59e3b18e313d21c5ff4bfe48bbd5e855445d57f4
NokkuPrudhvi <nokkuprudhvi.96@gmail.com>
2021-08-23 14:36:17 +0530

    added inference lambda

---
+++ inference/app.py
@@ -0,0 +1,89 @@
+import json
+import boto3
+# import requests
+
+import joblib
+
+model_name = '/opt/ml_model.joblib'
+model = joblib.load(model_name)
+
+def lambda_handler(event, context):
+    body = {
+        "message": "OK",
+    }
+
+    if 'queryStringParameters' in event.keys():
+        params = event['queryStringParameters']
+
+        medInc = float(params['medInc']) / 100000
+        houseAge = float(params['houseAge'])
+        aveRooms = float(params['aveRooms'])
+        aveBedrms = float(params['aveBedrms'])
+        population = float(params['population'])
+        aveOccup = float(params['aveOccup'])
+        latitude = float(params['latitude'])
+        longitude = float(params['longitude'])
+
+        inputVector = [medInc, houseAge, aveRooms, aveBedrms, population, aveOccup, latitude, longitude]
+        data = [inputVector]
+        #predictedPrice = model.predict(data)[0] * 100000 # convert to units of 1 USDs
+        client = boto3.client("sagemaker-runtime",region_name="us-east-1")
+
+        sample_payload = b"low,low,5more,more,big,high"
+
+        #sample_payload = data
+        response = client.invoke_endpoint(
+            EndpointName="cloudnokku-inference-endpoint", Body=sample_payload, ContentType="text/csv"
+        )
+
+        print("Our result for this payload is: {}".format(response["Body"].read().decode("ascii")))
+        predictedPrice
+        predictedPrice = round(predictedPrice, 2)
+        body['predictedPrice'] = predictedPrice
+
+    else:
+        body['message'] = 'queryStringParameters not in event.'
+
+    print(body['message'])
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body),
+        "headers": {
+            "Content-Type": 'application/json',
+            "Access-Control-Allow-Origin": "*"
+        }
+    }
+
+    return response
+
+def do_main():
+    event = {
+        'queryStringParameters': {
+            'medInc': 200000,
+            'houseAge': 10,
+            'aveRooms': 4,
+            'aveBedrms': 1,
+            'population': 800,
+            'aveOccup': 3,
+            'latitude': 37.54,
+            'longitude': -121.72
+        }
+    }
+
+    response = predict(event, None)
+    body = json.loads(response['body'])
+    print('Price:', body['predictedPrice'])
+
+    with open('event.json', 'w') as event_file:
+        event_file.write(json.dumps(event))
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "message": "hello world",
+            # "location": ip.text.replace("\n", "")
+        }),
+    }
+
+#do_main()
